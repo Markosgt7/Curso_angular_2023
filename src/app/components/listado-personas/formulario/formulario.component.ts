5ca49b9fd83c5f507245a4630cde3346739b1909
Markosgt7 <markos.guate7@gmail.com>
2023-08-02 16:48:34 -0600

    uso de viewChild con referencia local

--- src/app/components/listado-personas/formulario/formulario.component.ts
+++ src/app/components/listado-personas/formulario/formulario.component.ts
@@ -1,19 +1,37 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Persona } from '../persona.model';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
   @Output() personaCreada = new EventEmitter<Persona>();
   //nombreInput:string = '';
   //apellidoInput:string = '';
-
+  /* -------------------------------forma de realizar el proceso fon reference-----------------------
   agregarPersona(nombreInput:HTMLInputElement, apellidoInput: HTMLInputElement){
     let persona1 = new Persona(nombreInput.value, apellidoInput.value);
     this.personaCreada.emit(persona1);
   }
+ */
 
+  //------------------FORMA DE TRABAJAR VIEW CHILD-------------------------------------------
+
+  @ViewChild('nombreInput') nombreInput: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput: ElementRef;
+  agregarPersona() {
+    let persona1 = new Persona(
+      this.nombreInput.nativeElement.value,
+      this.apellidoInput.nativeElement.value
+    );
+    this.personaCreada.emit(persona1);
+  }
 }
